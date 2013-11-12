ce9415b855053d77c6cf9d322158acccbab2c122
andrew.probert@auckland.ac.nz <andrew.probert@auckland.ac.nz@6263ad75-0953-9372-c5b6-1b988e0050b6>
2013-11-12 01:35:44 +0000

    Fixed omission in the single test regression script

--- tests/bin/single_test.sh
+++ tests/bin/single_test.sh
@@ -45,7 +45,7 @@ export PROGNAME="pddl_main"
 
 python $PYREFORM_HOME/bin/$PROGNAME.py $PYREFORM_HOME/tests/$1/$2/input/domain.pddl $PYREFORM_HOME/tests/$1/$2/input/$2 > prog.out 2> prog.err
 
-DIFF_SIZE=`diff sas_plan_new /opt/pddl2/tests/$1/$2/output/sas_plan_new | wc -c`
+DIFF_SIZE=`diff sas_plan_new $PYREFORM_HOME/tests/$1/$2/output/sas_plan_new | wc -c`
 if [[ $DIFF_SIZE == '0' ]] 
 then
 	echo $1":    "$2 ": PASS"
